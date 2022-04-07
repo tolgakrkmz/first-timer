8654b07c5ffa04060bc99da5776aab781ebcc0bf
Tolga Korkmaz <tolgakrkmz7@gmail.com>
2022-04-07 17:37:31 +0300

    #10 When refresh page, making update state and add new function to repeated items.

--- timer.js
+++ timer.js
@@ -46,25 +46,24 @@ function formatHours(totalSeconds) {
 let listState = [];
 let detailsContainer;
 
-function handleSaveButton() {
-    // STEP 1: Apply to State.
-    let listItem = {
-        title: titleInputElement.value,
-        time: seconds,
-        description: descriptionField.value,
-        isDetailsShown: false,
-    };
+if (localStorage.getItem('list')) {
+    listState = JSON.parse(localStorage.getItem('list'));
 
-    listState.push(listItem);
+    // Setup initial list items.
+    for (const listItem of listState) {
+        addItemToDOM(listItem);
+    }
+}
 
-    // STEP 2: Resetting value of input elements.
+function addItemToDOM(listItem) {
+    // STEP 1: Reset value of input elements.
     descriptionField.value = '';
     titleInputElement.value = '';
 
-    // STEP 3: Create list item.
+    // STEP 2: Create list item.
     const listItemElement = document.createElement('li');
 
-    // STEP 4: Create list item title.
+    // STEP 3: Create list item title.
     const titleSpanElement = document.createElement('span');
     const separatorSpanElement = document.createElement('span');
 
@@ -76,14 +75,14 @@ function handleSaveButton() {
         separatorSpanElement.innerText = ' - ';
     }
 
-    // STEP 5: Create list item time.
+    // STEP 4: Create list item time.
     listItemElement.appendChild(document.createTextNode(formatHours(seconds)))
 
-    // STEP 6: Create 'Input' field for title.
+    // STEP 5: Create 'Input' field for title.
     const textInputBox = document.createElement('input');
     textInputBox.setAttribute('type', 'text');
 
-    // STEP 7: Create 'Edit' button.
+    // STEP 6: Create 'Edit' button.
     const titleEditButton = document.createElement('button');
     titleEditButton.innerText = 'Edit';
     titleEditButton.addEventListener('click', function () {
@@ -99,7 +98,7 @@ function handleSaveButton() {
 
     listItemElement.appendChild(titleEditButton);
 
-    // STEP 8: Create 'Save' button in Edit state.
+    // STEP 7: Create 'Save' button in Edit state.
     const titleSaveButton = document.createElement('button');
     titleSaveButton.innerText = 'Save';
     titleSaveButton.addEventListener('click', function () {
@@ -116,7 +115,7 @@ function handleSaveButton() {
         titleSaveButton.replaceWith(titleEditButton);
     });
 
-    // STEP 9: Create a 'Details' button.
+    // STEP 8: Create a 'Details' button.
     const detailsButton = document.createElement('button');
     detailsButton.innerText = 'Details';
 
@@ -159,7 +158,7 @@ function handleSaveButton() {
 
     listItemElement.appendChild(detailsButton);
 
-    // STEP 10: Create 'Remove' button.
+    // STEP 9: Create 'Remove' button.
     const removeButton = document.createElement('button');
     removeButton.innerText = 'Remove';
     removeButton.addEventListener('click', function () {
@@ -175,122 +174,27 @@ function handleSaveButton() {
 
     listItemElement.appendChild(removeButton);
 
-    // STEP 11: Add list item to list.
+    // STEP 10: Add list item to list.
     listElement.appendChild(listItemElement);
+}
 
+function handleSaveButton() {
+    // STEP 1: Apply to State.
+    let listItem = {
+        title: titleInputElement.value,
+        time: seconds,
+        description: descriptionField.value,
+        isDetailsShown: false,
+    };
+
+    listState.push(listItem);
+
+    // STEP 2: Persist to localStorage.
     localStorage.setItem('list', JSON.stringify(listState));
 
-};
-
-const itemByLocal = JSON.parse(localStorage.getItem('list'));
-
-for (const i of itemByLocal) {
-    const listItemElementLocal = document.createElement('li');
-    const titleSpanElementLocal = document.createElement('span');
-    const separatorSpanElementLocal = document.createElement('span');
-
-    titleSpanElementLocal.appendChild(document.createTextNode(i.title));
-    listItemElementLocal.appendChild(titleSpanElementLocal);
-    listElement.appendChild(listItemElementLocal)
-    listItemElementLocal.appendChild(separatorSpanElementLocal);
-
-    if (i.title.length > 0) {
-        separatorSpanElementLocal.innerText = ' - ';
-    }
-
-    listItemElementLocal.appendChild(document.createTextNode(formatHours(i.time)));
-    // Create Input box.
-    const textInputBox = document.createElement('input');
-    textInputBox.setAttribute('type', 'text');
-
-    // Title Edit Button. 
-    const titleEditButtonLocal = document.createElement('button');
-    titleEditButtonLocal.innerText = 'Edit';
-    titleEditButtonLocal.addEventListener('click', function () {
-        textInputBox.value = i.title;
-        titleSpanElementLocal.replaceWith(textInputBox);
-
-        if (textInputBox.value.length === 0) {
-            separatorSpanElementLocal.innerText = ' - ';
-        }
-
-        titleEditButtonLocal.replaceWith(titleSaveButtonLocal);
-    });
-
-    listItemElementLocal.appendChild(titleEditButtonLocal);
-
-    // Create Save Button in Edit state.
-    const titleSaveButtonLocal = document.createElement('button');
-    titleSaveButtonLocal.innerText = 'Save';
-    titleSaveButtonLocal.addEventListener('click', function () {
-        i.title = textInputBox.value;
-        localStorage.setItem('list', JSON.stringify(listState));
-        
-        titleSpanElementLocal.innerText = i.title;
-
-        if (i.title === 0) {
-            separatorSpanElementLocal.innerText = '';
-        }
-
-        textInputBox.replaceWith(titleSpanElementLocal);
-        titleSaveButtonLocal.replaceWith(titleEditButtonLocal);
-    });
-
-    // Create a 'Details' button.
-    const detailsButtonLocal = document.createElement('button');
-    detailsButtonLocal.innerText = 'Details';
-
-    detailsButtonLocal.addEventListener('click', function () {
-        // STEP 1: Handle already shown item.
-        const shownItem = itemByLocal.find(itemByLocal => itemByLocal.isDetailsShown);
-
-        if (shownItem !== undefined && shownItem !== itemByLocal) {
-            detailsContainer.remove();
-            shownItem.isDetailsShown = false;
-        }
-
-        // STEP 2: Handle currently clicked item.
-        if (!itemByLocal.isDetailsShown) {
-            detailsContainer = document.createElement('div');
-
-            const lineSeparator = document.createElement('hr');
-            detailsContainer.appendChild(lineSeparator);
-
-            const titleParagraphElement = document.createElement('p');
-            titleParagraphElement.innerText = `Title: ${i.title}`;
-            detailsContainer.appendChild(titleParagraphElement);
-
-            const descriptionParagraphElement = document.createElement('p');
-            descriptionParagraphElement.innerText = `Description: ${i.description}`;
-            detailsContainer.appendChild(descriptionParagraphElement);
-
-            const timeParagraphElement = document.createElement('p');
-            timeParagraphElement.innerText = `Time: ${formatHours(i.time)}`;
-            detailsContainer.appendChild(timeParagraphElement);
-
-            document.body.appendChild(detailsContainer);
-
-        } else {
-            detailsContainer.remove();
-        }
-
-        itemByLocal.isDetailsShown = !itemByLocal.isDetailsShown;
-    });
-
-    listItemElementLocal.appendChild(detailsButtonLocal);
-
-    // STEP 10: Create 'Remove' button.
-    const removeButton = document.createElement('button');
-    removeButton.innerText = 'Remove';
-    removeButton.addEventListener('click', function () {
-        localStorage.setItem('list', JSON.stringify(listState));
-
-        listItemElementLocal.remove();
-    });
-
-    listItemElementLocal.appendChild(removeButton);
-
-};
+    // STEP 3: Add list item to DOM.
+    addItemToDOM(listItem);
+}
 
 // Press Enter to Submit input.
 titleInputElement.addEventListener('keyup', function (event) {
@@ -307,4 +211,4 @@ descriptionField.addEventListener('keyup', function (event) {
     if (event.code === 'Enter') {
         handleSaveButton();
     }
-}); 
+});
